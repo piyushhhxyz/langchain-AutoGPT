c91ae50cdffd35edffb39c13a81375abc1f19df1
piyushhhxyz <piyush.bhawsar1@gmail.com>
2023-11-06 01:02:10 +0530

    BC API key push kardi mcbc

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
+from langchain.chains import LLMChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
